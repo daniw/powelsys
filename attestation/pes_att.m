4b1003817a8b45a819705c359c1731020c1aa5d9
daniw <dani.winz@bluewin.ch>
2017-11-09 20:58:24 +0100

    CCM analysis

--- attestation/pes_att.m
+++ attestation/pes_att.m
@@ -6,13 +6,20 @@ close all;
 % Load packages
 pkg load control
 
+% Detailed analysis
+detail = 0;
+
 % Boost converter parameters
 vo = 200;
 l = 200e-6;
 c = 5e-6;
 r = 50;
 fs = 100e3;
-vi = [50 100 150];
+if detail
+    vi = [1:1:200];
+else
+    vi = [50 100 150];
+end
 
 % Define internal variables
 s = tf("s");
@@ -20,5 +27,19 @@ s = tf("s");
 % Calculate static duty ratio
 d = 1 - (vi / vo);
 
-disp(["Duty ratios at different input voltages:"]);
-disp(num2str([vi; d]'))
+% Check for CCM
+ccm = d < 2 * (vo * l * fs)./(r * vi);
+
+if detail
+    figure(1);
+    plot(vi, d, "b", "LineWidth", 3, vi, 2 * (vo * l * fs)./(r * vi), "r", "LineWidth", 3);
+    grid on;
+    title("CCM analysis");
+    legend("Duty ratio", "Limit for CCM");
+    xlabel("V_i [V]");
+    ylabel("d [ ]");
+    ylim([0 5]);
+else
+    disp(["Duty ratios at different input voltages:"]);
+    disp(num2str([vi; d; ccm]'))
+end
