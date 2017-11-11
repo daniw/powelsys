ec985fc467ec9073009410b84578cc1975ff3d54
daniw <dani.winz@bluewin.ch>
2017-11-11 17:57:54 +0100

    Output changed

--- attestation/pes_att.m
+++ attestation/pes_att.m
@@ -1,3 +1,4 @@
+%% Preparation
 % Cleanup workspace
 clc;
 clear;
@@ -6,40 +7,59 @@ close all;
 % Load packages
 pkg load control
 
-% Detailed analysis
-detail = 1;
+% Define internal variables
+s = tf('s');
 
-% Boost converter parameters
+% Define plot and print output
+plotout = 1;
+printout = 1;
+
+%% Boost converter parameters
 vo = 200;
+%l = 62e-6;
 l = 200e-6;
 c = 5e-6;
 r = 50;
 fs = 100e3;
-if detail
-    vi = [1:1:200];
-else
-    vi = [50 100 150];
-end
+vi= [50 100 150];
+vi_plot = [1:1:200];
 
-% Define internal variables
-s = tf('s');
+% Display converter parameters
+disp(['Step up converter analysis']);
+disp(['']);
+disp(['Output voltage:      '   num2str(vo)     ' V']);
+disp(['Inductance:          '   num2str(l/1e-6) ' uH']);
+disp(['Output capacitor:    '   num2str(c/1e-6) ' uF']);
+disp(['Load resistance:     '   num2str(r)      ' Ohm']);
+disp(['Switching frequency: '   num2str(fs/1e3) ' kHz']);
+disp(['']);
 
+%% Duty ratio and CCM analysis
 % Calculate static duty ratio
-d = 1 - (vi / vo);
+d = 1 - (vi/ vo);
+d_plot = 1 - (vi_plot / vo);
 
 % Check for CCM
-ccm = d < 2 * (vo * l * fs)./(r * vi);
+d_max= 2 * (vo * l * fs)./(r * vi);
+ccm= d < d_max;
+d_max_plot = 2 * (vo * l * fs)./(r * vi_plot);
+ccm_plot = d_plot < d_max_plot;
 
-if detail
+% Display CCM for selected values
+disp(['Duty ratios at different input voltages:']);
+disp(num2str([vi; d; ccm]'))
+
+% Plot CCM over complete input voltage range
+if plotout
     figure(1);
-    plot(vi, d, 'b', 'LineWidth', 3, vi, 2 * (vo * l * fs)./(r * vi), 'r', 'LineWidth', 3);
+    plot(vi_plot, d_plot, 'b', 'LineWidth', 3, vi_plot, d_max_plot, 'r', 'LineWidth', 3, vi_plot, ccm_plot, 'g', 'LineWidth', 2);
     grid on;
     title('CCM analysis');
-    legend('Duty ratio', 'Limit for CCM');
+    legend('Duty ratio', 'Limit for CCM', '1=CCM, 0=DCM');
     xlabel('V_i [V]');
     ylabel('d [ ]');
-    ylim([0 5]);
-else
-    disp(['Duty ratios at different input voltages:']);
-    disp(num2str([vi; d; ccm]'))
+    ylim([0 3]);
+    if printout
+        print -dpdf fig/ccm.pdf
+    end
 end
